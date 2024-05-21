7854d8c138ba379a412118fc20f920fd8ea19ce5
M Sami <beboolean@gmail.com>
2024-05-22 00:51:05 +0530

    Task 1

---
+++ Task 1/index.js
@@ -0,0 +1,10 @@
+// Write your code below:
+const heading=document.getElementById("main-heading")
+  heading.textContent="Fruit World";
+heading.style.color="orange";
+const divhead=document.getElementById("header");
+divhead.style.backgroundColor="green";
+divhead.style.borderBottom="4px solid orange";
+
+document.getElementById("basket-heading").style.color="green";
+document.getElementById("thanks").innerHTML="<p>Please visit us again</p>"
